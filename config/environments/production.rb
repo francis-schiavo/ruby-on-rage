9d19ed522a1013a69ca395ffb2e86d787944233b
Francis Schiavo <francischiavo@gmail.com>
2026-03-23 21:12:20 -0300

    feat: Add oficial DNS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,11 +82,8 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [:id]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
-  #
+  config.hosts = %w[www.rubyonrage.com rubyonrage.com www.rubyonrage.org rubyonrage.org]
+
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
